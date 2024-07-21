834e2f8a170652fd8a0c7df72df42c17ab69f58b
Arnaud Bailly <arnaud.bailly@iohk.io>
2024-07-21 12:53:34 +0200

    Can create a new battle and access battle page

--- pacific-war/pacific-war/src/main.rs
+++ pacific-war/pacific-war/src/main.rs
@@ -9,9 +9,41 @@ fn index() -> Template {
     Template::render("index", context! {})
 }
 
+#[get("/battle?<battle_name>&<start_date>&<duration>")]
+fn battle(battle_name: &str, start_date: &str, duration: u8) -> Template {
+    Template::render(
+        "battle",
+        context! {
+        battle_name,
+        start_date,
+        duration,},
+    )
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .attach(Template::fairing())
-        .mount("/", routes![index])
+        .mount("/", routes![index, battle])
+}
+
+#[cfg(test)]
+mod test {
+    use super::rocket;
+    use rocket::http::Status;
+    use rocket::local::blocking::Client;
+
+    #[test]
+    fn hello_world() {
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+        let mut response = client
+            .get("/battle?battle_name=Coral+Sea&start_date=1942-05-01&duration=21")
+            .dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        let response_string = response.into_string().unwrap();
+        assert!(response_string.contains("Coral Sea"));
+        assert!(response_string.contains("1942-05-01"));
+        assert!(response_string.contains("Current date: 1942-05-01"));
+        assert!(response_string.contains("21"));
+    }
 }
